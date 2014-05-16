9f937865c39804882a74918fce449b05031491b7
Mac Liu <macliu@Macs-MacBook-Air.local>
2014-05-15 23:59:23 -0500

    Initial Commit

---
+++ Fuse/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Fuse
+//
+//  Created by Mac Liu on 5/15/14.
+//  Copyright (c) 2014 Mac Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Fuse/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Fuse
+//
+//  Created by Mac Liu on 5/15/14.
+//  Copyright (c) 2014 Mac Liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
